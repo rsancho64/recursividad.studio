ea49c40ad147750b0832d8f0024a6d683cc8ae01
rsancho64 <rsancho@iesrioarba.es>
2023-11-22 12:49:43 +0100

    .

--- sumatorio.py
+++ sumatorio.py
@@ -2,8 +2,11 @@ from esListaEnterosValida import esListaEnterosValida
 
 
 def sumatorio(lista):
+    """sumatorio iterativo lista enteros"""
+
     if not esListaEnterosValida(lista):
-        raise ValueError("lista chunga")
+        raise ValueError(f"lista chunga: {lista}")
+
     total = 0
     for item in lista:
         total += item
@@ -11,23 +14,22 @@ def sumatorio(lista):
 
 
 def sumatorioREC(lista):
+    """sumatorio recursivo lista enteros"""
 
     if not esListaEnterosValida(lista):
-        raise ValueError("lista chunga")
+        raise ValueError(f"lista chunga: {lista}")
 
-    if not lista:
-        return 0  # trivial 0
-    if 1 == len(lista):
-        return lista[0]  # trivial 1
+    if not lista: return 0               # trivial 0
+    if 1 == len(lista): return lista[0]  # trivial 1
     return lista[0] + sumatorioREC(lista[1:])
 
 
 if __name__ == "__main__":
 
     lista = [11, 22, 33, 44, 55]     # 0.. 11..  33..  66.. 110.. 165 (total)
-    lista = [11, 22, "cucu", 44, 55]
-    lista = [11]
-    lista = []
+    # lista = [11, 22, "cucu", 44, 55]
+    # lista = [11]
+    # lista = []
 
-    # print(sumatorio(lista))  ## 165
+    print(sumatorio(lista))  ## 165
     print(sumatorioREC(lista))  # 165
